e3dfc0c6da782f787dd1704f12c4f4ed7cbc5f33
Matt Borja <matt.borja@gmail.com>
2014-01-21 09:05:14 -0700

    Catch 'Invalid hex string' when verifying signatures

--- examples/signing.js
+++ examples/signing.js
@@ -1,13 +1,16 @@
 /* jslint node: true */
 'use strict';
 
-var CryptoWrapper = require('../lib/crypto-wrapper')
+var CryptoWrapper = require('../lib/crypto-wrapper');
 var crypto = new CryptoWrapper();
 
-var data = 'Hi John!\n\nIt was nice meeting up with you at CryptoCon this past weekend. Here is my public key:\nABCDEF123\n\nBob'
+var data = 'Hi John!\n\nIt was nice meeting up with you at CryptoCon this past weekend. Here is my public key:\nABCDEF123\n\nBob';
+var data_sig = crypto.Sign(data);
 
-console.log('=== BEGIN MESSAGE ===')
+console.log('=== BEGIN MESSAGE ===');
 console.log(data);
 console.log('=== SIGNATURE ===');
-console.log(crypto.Sign(data));
+console.log(data_sig);
 console.log('=== END MESSAGE ===');
+
+console.log('Valid signature: ' + crypto.Verify(data, data_sig));
